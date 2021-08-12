727979fa2eb16820336b49b66eddc5bfacacc434
Ernesto Gomez <Ernesto.Gomez@endava.com>
2021-08-12 15:13:56 -0300

    Solved quit code repetitions on search

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -3,20 +3,14 @@ describe('Search elements',()=>{
         cy.visit('/');  //se escribe asi y no se especifica la url porque lo toma del baseUrl del cypress.json
     })
     it('search elements with multiple results', () =>{
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('dress');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('dress')
         cy.fixture('searchResults').then((searchResults)=>{
             cy.get(searchResults.title).should('contain','dress')
         })
     })
 
     it('search for elements with no results', ()=>{
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('querty');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('querty')
         cy.fixture('searchResults').then((searchResults)=>{
             cy.get(searchResults.alert).should('contain','No results were found for your search')
         })  
